41116e921489a7ef6bdced85fbdd456d7fd53a6a
ALX <alexandreaddres@gmail.com>
2022-08-23 23:44:15 -0300

    Applying heritage and polymorfism with str buildin

--- programs.py
+++ programs.py
@@ -1,6 +1,6 @@
-# Criar duas classes [filme e serie]com atributos e métodos para seus conteúdos
+# A ideia é criar uma playlist de filmes e séries
 
-#Criar classe generalista para aplicar herança
+# Criar classe generalista para aplicar herança
 class Midia:
 
     def __init__(self, name_midia, year_launch):
@@ -17,6 +17,25 @@ class Midia:
     def set_likes(self):
         self._likes_number += 1
 
+    def __str__(self):
+        return f'Filme: {self._name} \n' f'Ano: {self.year}\n' f'Temporadas: {self.year}\n' f'Número de likes: {self._likes_number}\n'
+
+
+# Criar uma classe que mostre o tamanho da minha playlist e retorne os nomes
+class Playlist:
+    def __init__(self, name_playlist, playlists):
+        self.name_playlist = name_playlist
+        self.playlists = playlists
+
+    @property
+    def get_return_name_playlist(self):
+        return self.name_playlist
+
+    @property
+    def return_number_midias(self):
+        return len(self.playlists)
+
+
 # Criar classe filme deve ter os atributos nome do filme, ano, duração. Aplicar herança
 
 class Movie(Midia):
@@ -25,13 +44,11 @@ class Movie(Midia):
         super().__init__(name_midia, year_launch)
         self.time = time
 
-    def show_movie(self):
-        print(f'Filme: {self._name} \n'
-              f'Ano: {self.year}\n'
-              f'Duração: {self.time}\n'
-              f'Número de likes: {self._likes_number}')
+    def __str__(self):
+        return f'Filme: {self._name} \n' f'Ano: {self.year}\n' f'Temporadas: {self.year}\n' f'Número de likes: {self._likes_number}\n'
 
-#Crie a classe das séries com nome, ano e temporada e aplicar herança
+
+# Crie a classe das séries com nome, ano e temporada e aplicar herança
 
 class Serie(Midia):
 
@@ -39,22 +56,31 @@ class Serie(Midia):
         super().__init__(name_midia, year_launch)
         self.seasons = numbers_season
 
-    def show_series(self):
-        print(f'Filme: {self._name} \n'
-              f'Ano: {self.year}\n'
-              f'Temporadas: {self.seasons}\n'
-              f'Número de likes: {self._likes_number}')
+    def __str__(self):
+        return f'Filme: {self._name} \n' f'Ano: {self.year}\n' f'Temporadas: {self.seasons}\n' f'Número de likes: {self._likes_number}\n'
 
-#Settar as informações
+
+# Settar as informações
 vingadores = Movie('vingadores - guerra infinita', 2018, 160)
+vingadores.set_likes()
+vingadores.set_likes()
+vingadores.set_likes()
+
 atlanta = Serie('atlanta', 2018, 2)
-vingadores.set_likes()
-vingadores.set_likes()
-vingadores.set_likes()
-
 atlanta.set_likes()
 atlanta.set_likes()
 
-#Chamar o programa
-print(f'Nome: {vingadores._name} - Likes: {vingadores._likes_number}')
-print(f'Nome: {atlanta._name} - Likes: {atlanta._likes_number}')
+
+
+# Aplicar Polimorfismo no módulo - implementando playlist
+
+list_midias = [vingadores, atlanta]
+
+# Criar o objeto playlist
+minha_playlist = Playlist('fim de semana', list_midias)
+
+
+for midia in minha_playlist.playlists:
+    print(midia)
+
+print(f'Playlist: {minha_playlist.name_playlist}. \n Possui: {minha_playlist.return_number_midias} séries/filmes')
